789fd37db3dce3aa76fb2e39a4b1bec422f59e39
Marek Jakubicki <jakubicki.m@gmail.com>
2020-05-13 20:13:22 +0200

    middlers.pl

---
+++ footerBar.js
@@ -0,0 +1,14 @@
+var html = '<footer>\
+<p class="footerText">\
+    ○●○ middlers | mobilny doradca finansowy we Wrocławiu\
+</p>\
+<a class="footerText" href="https://middlers.pl/polityka-prywatnosci.html">Polityka prywatności</a>\
+<a class="footerText" href="https://middlers.pl/polityka-cookies.html">Polityka cookies</a>\
+<p class="footerText">\
+    Middlers Sp. z o.o. z siedzibą we Wrocławiu ul. Leszczyńskiego 4/29, 50-078 Wrocław, zarejestrowana w Sądzie\
+    Rejonowym dla Wrocławia-Fabrycznej KRS-532823, NIP-8971803272, REGON-360170795, kapitał zakładowy 5.000,00\
+    zł.\
+</p>\
+</footer>';
+
+document.getElementById('footerBar').innerHTML = html;
---
+++ navigationBar.js
@@ -0,0 +1,27 @@
+var html = '<nav class="navbar">\
+<span class="navbar-toggle" id="js-navbar-toggle">\
+    <div class="hamburger"></div>\
+    <div class="hamburger"></div>\
+    <div class="hamburger"></div>\
+</span>\
+<a href="../" class="logo">○●○ middlers</a>\
+<ul class="main-nav" id="js-menu">\
+    <li>\
+        <a href="/#korzysci" class="nav-links">KORZYŚCI </a>\
+    </li>\
+    <li>\
+        <a href="/#etapy" class="nav-links">PROCES</a>\
+    </li>\
+    <li>\
+        <a href="/#testimonials" class="nav-links">OPINIE</a>\
+    </li>\
+    <li>\
+        <a href="/blog" class="nav-links">BLOG</a>\
+    </li>\
+    <li>\
+        <a href="/kontakt" class="nav-links">KONTAKT</a>\
+    </li>\
+</ul>\
+</nav>';
+
+document.getElementById('navigationBar').innerHTML = html;
